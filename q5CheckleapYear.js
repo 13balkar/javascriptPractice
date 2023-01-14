d0b8fa5637cd0c65c1d8983a709c0bfad1e55eb5
Balkar_Singh <balkar81199@gmial.com>
2023-01-15 00:30:21 +0530

    feat: check if year is leap or not

---
+++ q5CheckleapYear.js
@@ -0,0 +1,8 @@
+function checkLeap(year){
+    return year%400===0 ? (true) : (year%100===0 ? false: (year%4===0 ? true :false));
+}
+ const checkLeaps= year=>{
+    return year%400===0 ? (true) : (year%100===0 ? false: (year%4===0 ? true :false));
+ };
+console.log(checkLeap(2001));
+console.log(checkLeaps(2000));
