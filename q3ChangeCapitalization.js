d7dbea1b1fb5ff07035522e27610551ed3da10b1
Balkar_Singh <balkar81199@gmial.com>
2023-01-15 00:16:21 +0530

    feat: change uppercase to lowecase and vice-versa for string

---
+++ q3ChangeCapitalization.js
@@ -0,0 +1,16 @@
+function change(input){
+    let output="";
+    [...input].forEach(inputChar=>{
+        output += inputChar===inputChar.toUpperCase() ? inputChar.toLowerCase():inputChar.toUpperCase();
+    });
+    return output;
+}
+const changes=(input)=>{
+    let output="";
+    [...input].forEach(inputChar=>{
+        output += inputChar===inputChar.toUpperCase() ? inputChar.toLowerCase():inputChar.toUpperCase();
+    });
+    return output;
+}
+console.log(change("balKAR"));
+console.log(changes("SiNgH"))
