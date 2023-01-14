7dea58113f318c696d4a1b85fe1d4cf8b80a93a1
Balkar_Singh <balkar81199@gmial.com>
2023-01-15 00:22:17 +0530

    feat: find the longest string of array

---
+++ q4LongestString.js
@@ -0,0 +1,26 @@
+function longestString(input){
+    let maxLength=0;
+    let output="";
+    input.forEach(itr=> {
+        let length=itr.length;
+        if(length>maxLength){
+            maxLength=length;
+            output=itr;
+        }
+    });
+    return output;
+}
+const longString=(input)=>{
+    let maxLength=0;
+    let output="";
+    input.forEach(itr=> {
+        let length=itr.length;
+        if(length>maxLength){
+            maxLength=length;
+            output=itr;
+        }
+    });
+    return output;
+}
+console.log(longestString(["we", "love", "codesssss", "academy"]));
+console.log(longString(["we", "love", "codesssss", "academy"]));
