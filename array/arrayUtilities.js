86e96c54a0e9aeafe59ecd6aadb2f28c9755f291
Balkar_Singh <balkar81199@gmial.com>
2023-01-18 12:46:44 +0530

    feat: add test cases

--- array/arrayUtilities.js
+++ array/arrayUtilities.js
@@ -1,66 +1,83 @@
-const containsOnlyNumbers=(array)=>{
-  return array.every((element)=>{
-    return typeof element==='number';
+const containsOnlyNumbers = (array) => {
+  return array.every((element) => {
+    return typeof element === 'number';
   })
 }
 
-const doubleArray= (numbers)=>{
-  if(!Array.isArray(numbers)){
+const doubleArray = (numbers) => {
+  if (!Array.isArray(numbers)) {
     throw new Error('Not an array');
   }
-  if(numbers.length===0){
+  if (numbers.length === 0) {
     throw new Error('Empty error');
   }
-  if(!containsOnlyNumbers(numbers)){
+  if (!containsOnlyNumbers(numbers)) {
     throw new Error('All elements of array are not integers')
   }
-  return numbers.map( number => number*2);
+  return numbers.map(number => number * 2);
 };
-const getEvenNumberArray= numbers=>{
-  if(!Array.isArray(numbers)){
-      throw new Error('Not an array');
-  }
-  if(numbers.length===0){
-    throw new Error('Empty error');
-  }
-  if(!containsOnlyNumbers(numbers)){
-    throw new Error('All elements of array are not integers')
-  }
-  return numbers.filter(num=> num%2===0);
-}
-
-const getEvenTripleNumber=(numbers)=>{
-  if(!Array.isArray(numbers)){
+const getEvenNumberArray = numbers => {
+  if (!Array.isArray(numbers)) {
     throw new Error('Not an array');
   }
-  if(numbers.length===0){
+  if (numbers.length === 0) {
     throw new Error('Empty error');
   }
-  if(!containsOnlyNumbers(numbers)){
+  if (!containsOnlyNumbers(numbers)) {
     throw new Error('All elements of array are not integers')
   }
-  const tripleNumbers=numbers.map(number=>number*3);
-  return tripleNumbers.filter(number => number%2===0);
+  return numbers.filter(num => num % 2 === 0);
 }
 
 
-const getEvenTripleNumbers=(numbers)=>{
-  if(!Array.isArray(numbers)){
+
+
+const getEvenTripleNumber = (numbers) => {
+  if (!Array.isArray(numbers)) {
     throw new Error('Not an array');
   }
-  if(numbers.length===0){
+  if (numbers.length === 0) {
     throw new Error('Empty error');
   }
-  if(!containsOnlyNumbers(numbers)){
+  if (!containsOnlyNumbers(numbers)) {
     throw new Error('All elements of array are not integers')
   }
-  const evenNumbersArray=numbers.reduce((evenNumbers,number)=>{
-    if(number%2==0){
-      evenNumbers.push(3*number);
+  const tripleNumbers = numbers.map(number => number * 3);
+  return tripleNumbers.filter(number => number % 2 === 0);
+}
+
+
+
+
+
+
+const getEvenTripleNumbers = (numbers) => {
+  if (!Array.isArray(numbers)) {
+    throw new Error('Not an array');
+  }
+  if (numbers.length === 0) {
+    throw new Error('Empty error');
+  }
+  if (!containsOnlyNumbers(numbers)) {
+    throw new Error('All elements of array are not integers')
+  }
+  const evenNumbersArray = numbers.reduce((evenNumbers, number) => {
+    if (number % 2 == 0) {
+      evenNumbers.push(3 * number);
     }
     return evenNumbers;
-  },[])
+  }, [])
   return evenNumbersArray;
 }
 
-module.exports= {doubleArray,getEvenNumberArray,getEvenTripleNumber,getEvenTripleNumbers};
+
+const getEvenTripleNumbers2= numbers => numbers.reduce(
+  (acc,current)=> {
+    if(current%2===0){
+      acc.push(3*current);
+    }
+    return acc;
+  },[])
+
+  console.log(getEvenTripleNumbers2([2,4,6,8]));
+module.exports = { doubleArray, getEvenNumberArray, getEvenTripleNumber, getEvenTripleNumbers };
--- q3ChangeCapitalization.js
+++ q3ChangeCapitalization.js
@@ -1,14 +1,14 @@
-function change(input){
-  let output='';
-  [...input].forEach(inputChar=>{
-    output += inputChar===inputChar.toUpperCase() ? inputChar.toLowerCase():inputChar.toUpperCase();
+function change(input) {
+  let output = '';
+  [...input].forEach(inputChar => {
+    output += inputChar === inputChar.toUpperCase() ? inputChar.toLowerCase() : inputChar.toUpperCase();
   });
   return output;
 }
-const changes=(input)=>{
-  let output='';
-  [...input].forEach(inputChar=>{
-    output += inputChar===inputChar.toUpperCase() ? inputChar.toLowerCase():inputChar.toUpperCase();
+const changes = (input) => {
+  let output = '';
+  [...input].forEach(inputChar => {
+    output += inputChar === inputChar.toUpperCase() ? inputChar.toLowerCase() : inputChar.toUpperCase();
   });
   return output;
 };
@@ -16,4 +16,4 @@ console.log(change('balKAR'));
 console.log(changes('SiNgH'));
 
 
-module.exports= {change,changes};
+module.exports = { change, changes };
